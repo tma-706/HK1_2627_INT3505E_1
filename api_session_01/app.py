89641fe9fc777112387c1676e339b718214e92fc
tma-706 <24020027@vnu.edu.vn>
2026-09-11 00:32:45 +0700

    Practice exercise 2 - session 1

--- api_session_01/app.py
+++ api_session_01/app.py
@@ -1,9 +1,18 @@
-from flask import Flask 
+from flask import Flask, jsonify, request
+#Do flask 3.x.x đã tự chuyển dict sang json nên không cần jsonify, ở đây chỉ thêm để phân biệt
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return {'message': 'Hello, API!'}
 
+#GET
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({'status': 'ok'}), 200
+
+#POST
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json(silent=True) or {}
+    return jsonify({"You sent": data}), 200
+    
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
